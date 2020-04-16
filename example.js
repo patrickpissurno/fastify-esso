92d20349071f053445c338144ed6069a7a06fa4b
Diego de Oliveira <dodiego@gmail.com>
2020-04-16 00:55:45 -0300

    - added default nodejs gitignore - added type options - extended fastify request type with auth property - extended fastify instance with generateAuthToken and requireAuthentication - added jsconfig to have better intellisense and type checking - added eslint with standardjs

--- example.js
+++ example.js
@@ -1,33 +1,37 @@
-const fastify = require('fastify')();
-const err = require('http-errors');
+const fastify = require("fastify")();
+const err = require("http-errors");
 
-fastify.register(require('.')({ secret: '11111111111111111111' }));
+fastify.register(require(".")({ secret: "11111111111111111111" }));
 
 /** @param { import('fastify').FastifyInstance } fastify */
-async function routes(fastify){
-    fastify.get('/', (req, reply) => reply.send({ public: true }));
-    
-    fastify.post('/auth', async (req, reply) => {
-        const valid_credentials = req.body.user === 'John' && req.body.password === '123'; //never use this in production (unsafe). instead, use crypto.timingSafeEqual
-        if(!valid_credentials)
-            throw new err.Forbidden();
+async function routes(fastify) {
+  fastify.get("/", async (req, reply) => reply.send({ public: true }));
 
-        const token = await fastify.generateAuthToken({ user: req.body.user }); // you can add as much data as you want here, but keep in mind: the less, the faster
-        //Bearer 2ead6b62dec72e4c5c50a0e3578fc746e29eb2d374b9d21f655cc35710d09c99
+  fastify.post("/auth", async (req, reply) => {
+    const valid_credentials =
+      req.body.user === "John" && req.body.password === "123"; //never use this in production (unsafe). instead, use crypto.timingSafeEqual
+    if (!valid_credentials) throw new err.Forbidden();
 
-        return { token };
-    });
+    const token = await fastify.generateAuthToken({ user: req.body.user }); // you can add as much data as you want here, but keep in mind: the less, the faster
+    //Bearer 2ead6b62dec72e4c5c50a0e3578fc746e29eb2d374b9d21f655cc35710d09c99
 
-    fastify.register(privateRoutes);
+    return { token };
+  });
+
+  fastify.register(privateRoutes);
 }
 
 /** @param { import('fastify').FastifyInstance } fastify */
-async function privateRoutes(fastify){
-    fastify.requireAuthentication(fastify);
+async function privateRoutes(fastify) {
+  fastify.requireAuthentication(fastify);
 
-    fastify.get('/test', (req, reply) => reply.send({ private: true, message: 'Hello, ' + req.auth.user + '!' }));
+  fastify.get("/test", async (req, reply) =>
+    reply.send({ private: true, message: "Hello, " + req.auth.user + "!" })
+  );
 }
 
 fastify.register(routes);
 
-fastify.listen(3000, '0.0.0.0', (err) => console.log(err ? err : 'Listening at 3000'));
+fastify.listen(3000, "0.0.0.0", (err) =>
+  console.log(err ? err : "Listening at 3000")
+);
